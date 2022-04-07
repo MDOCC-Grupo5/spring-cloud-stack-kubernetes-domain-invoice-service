06c095a3621bea93820927f70a103b1314f58c5a
Juan Sebastian Suaza Solano <juan.suaza@api-ux.com>
2022-04-06 23:26:30 -0500

    Feat: Configure project Update Dockerfile, replace bootstrap.yml to application.yml, remove unused libraries in pom.xml and add schema-postgres.sql in resources

---
+++ src/main/resources/schema-postgres.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS public.invoice (
+    id bigserial primary key,
+    user_id integer null,
+    created_at timestamp not null,
+    total_amount double precision not null
+);
