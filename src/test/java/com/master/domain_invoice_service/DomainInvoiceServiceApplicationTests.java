5edb3ec69607501d1517f648c8335b89bd254033
Juan Sebastian Suaza Solano <juan.suaza@api-ux.com>
2022-04-08 12:21:04 -0500

    Fix: test step Add h2 database

--- src/test/java/com/master/domain_invoice_service/DomainInvoiceServiceApplicationTests.java
+++ src/test/java/com/master/domain_invoice_service/DomainInvoiceServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.master.domain_invoice_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class DomainInvoiceServiceApplicationTests {
 
     @Test
